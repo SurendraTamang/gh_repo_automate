b90ba97fdabac9f7ce26c19c0b980b06d97dd8d1
surendra <tamangsurendra44@gmail.com>
2023-08-06 06:14:04 +0000

    Update the command for testing

---
+++ runner.py
@@ -0,0 +1,35 @@
+import csv
+import subprocess
+# Read the csv file 
+
+CSV_FILE = "projects.csv"
+
+
+def main():
+    with open(CSV_FILE) as projects_file:
+        projects = csv.DictReader(projects_file)
+        for project in projects:
+            process = create_repository(project)
+            subprocess.run(process, shell=True, check=True)
+
+
+
+
+# Call the process
+# gh repo create "scrapians/Facebook Posts Scraper" --private --description "Scrapes the Public facebook posts"
+def create_repository(details):
+    organization = details.get('organization',False)
+    repository_name = details.get('respository_name')
+    visibiliy = details.get('visibility','--private')
+    description = details.get('description',repository_name)
+    if organization:
+        repository_name = f'{organization}/{repository_name}'
+
+    if not visibility.startswith('--'):
+        visibility = '--'+visibility
+    
+    process = ['gh','repo', 'create', repository_name,visibiliy,'--description', description]
+    return process
+
+
+
